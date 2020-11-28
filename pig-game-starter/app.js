19d80160d642bd134a5ec92190c2d968e53de2dc
Jerickc Camacho <jerickcamacho42@gmail.com>
2020-11-28 11:09:15 +0800

    Pig-Game-Starter

--- pig-game-starter/app.js
+++ pig-game-starter/app.js
@@ -6,93 +6,159 @@ GAME RULES:
 - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
-
 */
 
-// Defining Variables 
-let activePlayer, gamePlaying, defaultScores, roundScore; 
+// Assigning variables using array  
+let activePlayer, globalScores, roundScore, gamePlay; 
 
-// Initializing new game 
-newGame(); 
-
-// executing functions of newGame()
+// executing functions 
 document.querySelector(".btn-new").addEventListener("click", newGame); 
+document.querySelector(".btn-roll").addEventListener("click", rollDice); 
+document.querySelector(".btn-hold").addEventListener("click", holdScore); 
 
-// document.querySelector(".btn-hold").addEventListener("click" ,); 
+/***** Targeting elements to manipulate and select by using Query Selector *****/  
+console.log(document.querySelector(".wrapper ")); // Whole div 
+console.log(document.querySelector(".btn-element")); // whole button includes = button, image and input 
 
-/*********************** NEW GAME function ****************************/
-function newGame() {   
-    scores = [0 , 0]; // starting array defaultScores 
-    activePlayer = 0; // Player 1
-    roundScore = 0;  // hold button score 
-    gamePlaying = true; // start game 
+/**************** PLAYER 1 **********************/
+console.log(document.querySelector(".player-0-panel")); 
+console.log(document.querySelector(".player-current-box")); 
 
-    // DOM selection  
-    const Form = document.getElementById("form");  // Si getElementByID kunin nya elements from html 
-     
-    // DOM manipulation
-    const Inputs = document.querySelectorAll(".btn-element"); 
+/**************** PLAYER 2 **********************/
+console.log(document.querySelector(".player-1-panel"));
+console.log(document.querySelector(".player-current-box")); 
 
-    // Remove the image displaying none, mag show lang kapag nag roll dice (Dice Image display)
-    document.querySelector(".dice-one").style.display = "none"; 
-    document.querySelector(".dice").style.display = "none"; 
+// Player 1 and Player 2 class = player-score change both value into '0' 
+let playerScore = document.querySelectorAll(".player-score"); 
 
-    // Right side "Player 2" 
-    const Player1Panel = document.querySelector(".player-1-panel"); 
-    document.querySelector("#score-1").textContent = '0'; // Change the text display of id = "score-1" from 72 to "0"
-    document.querySelector('#name-1').textContent = 'Player 2'; // to remove 'winner' text for the new game
-    document.querySelector('.player-1-panel').classList.remove('active'); // reset related line in function holdScore
-    document.querySelector('.player-1-panel').classList.remove('winner'); // reset related line in function holdScore
+// Targeting class = player-current-score to change '0' once hit the newGame button 
+let playerCurrentScore = document.querySelectorAll(".player-current-score"); 
 
+// Targeting class = player-name  
+let player1Name = document.querySelector("#name-0"); 
+let player2Name = document.querySelector("#name-1"); 
 
-    // Left side "Player 1" 
-    const Player0Panel = document.querySelector(".player-0-panel"); 
-    document.querySelector("#score-0").textContent = '0'; // Change the text display of id = "score-0" from 43 to "0"
-    document.querySelector("#current-0").textContent = '0'; // Change the text display of id = "current-0" from 11 to "0" 
-    document.querySelector('#name-0').textContent = 'Player 1'; // to remove 'winner' text for the new game
-	document.querySelector('.player-0-panel').classList.remove('active'); // reset related line in function holdScore
-    document.querySelector('.player-0-panel').classList.remove('winner'); // reset related line in function holdScore
-    
-    document.querySelector('.player-0-panel').classList.add('active'); // highlight on Player 1 by default
+// Targeting Player 1 class = player-0-panel and Player 2 class = player-1-panel// Targeting Player 1 class = player-0-panel and Player 2 class = player-1-panel
+let player0Panel = document.querySelector(".player-0-panel"); 
+let player1Panel = document.querySelector(".player-1-panel"); 
 
-console.log(Form);
-console.log(Player1Panel);
-console.log(Player0Panel);
-console.log(Inputs);  
-}
-//*********************** End of NEW GAME function Button ****************************/
+// Targeting Image dice from html and Threshold 
+let threshold = document.querySelector("#threshold"); 
+let imgDice = document.querySelector(".dice");  
+ 
+/*********************** Start of NEW GAME function ****************************/
+    function newGame(){
+        globalScores = [0,0]; 
+        roundScore = 0; 
+        activePlayer = 0; 
+        gamePlay = true; 
 
-// executing functions of rollDice()
-document.querySelector(".btn-roll").addEventListener("click" , rollDice); 
-//*********************** Roll Dice function Button ****************************/
-function rollDice(){
-    if(gamePlaying){
+        // 
+         threshold.addEventListener("keypress", function(){ // inserting the value of threshold 
+             threshold.value;   
+         })
+
+         threshold.value = threshold.defaultValue; // Kapag nirefresh babalik sa dating value 
+         imgDice.style.display = "none"; 
+
+        // Player 1 and Player 2 class = player-score change both value into '0' 
+        for(let globalScore of playerScore){
+            console.log(globalScore.textContent = '0');  
+        }
+        // Targeting class = player-current-score to change '0' once hit the newGame button 
+        for(let playerCurrentScores of playerCurrentScore){
+            console.log(playerCurrentScores.textContent = '0'); 
+        }
+        // Kapag nag hit new Game mag rereset sa name below indicated value 
+        player1Name.textContent = "Player 1"; 
+        player2Name.textContent = "Player 2";  
+
+        // Player 1 remove the active and winner 
+        player0Panel.classList.remove('active'); 
+        player0Panel.classList.remove('winner'); 
+
+        // Player 2 remove the active and winner
+        player1Panel.classList.remove('active'); 
+        player1Panel.classList.remove('winner'); 
+
+        // Highlithed the Panel Player 1 as active 
+        player1Panel.classList.add('active');
         
-        let diceResult = Math.floor(Math.random() * 6) + 1; // Eto yung formula para makuha yung dice once na magrollDice
-        
-        let dice = document.querySelector(".dice"); // image class = dice // assigning variable
-        dice.style.display = 'block'; // adjusting css 
-        dice.src = `dice-${diceResult}.png`; // linking the picture to be used
+    }
+    newGame();
+    console.log(threshold);      
 
-        
-         let diceOne = document.querySelector(".dice-one"); // image class = dice-one 
-         diceOne.display = 'block'; 
-         diceOne.src = `dice-${diceOne}.png`; 
+//*********************** End of NEW GAME function Button ****************************/  
 
-        if(diceResult !== 1){
-            roundScore += diceResult; // 
-            document.querySelector(`#current-${activePlayer}`).textContent = roundScore; 
-        }else{
-            nextPlayer(); 
+/*********************** ROLL DICE function Button **********************************/  
+    function rollDice(){
+        let currenRoundScores = document.querySelector(`#current-${activePlayer}`); // Assigning variable for the current scores
+
+        if(gamePlay){
+            let diceResult = Math.floor(Math.random() * 6) + 1; //  Getting dice output as dice result using this formula 
+            imgDice.style.display = "block"; // adjusting the css display property into block
+            imgDice.src = `dice-${diceResult}.png`; // the image display wil depends on the result of dice 
+
+            if(diceResult === 1){  // statement condition once the roll dice hit number equal to  '1' switch turn to the ned Player
+                nextPlayer(); // initiate next player turn 
+                imgDice.style.display = "none"; // nothings will be displayed 
+            
+            } else {
+                roundScore += diceResult; // to get the sum of rounScore by using this formula 
+                currenRoundScores.textContent = roundScore; // then yung currenScore ididisplay nya yung roundScore 
+            }
         }
     }
-}
+/*********************** End of ROLL DICE function Button ****************************/ 
 
+/*********************** Start of Taking Turns ****************************/
+    function nextPlayer(){
+        if(activePlayer === 0){ // // kapag si Player 1 ay '0' active Player, tapos nextPlayer magiging Player 2 '1'
+            activePlayer = 1; 
+            roundScore = 0; // removing this would pass the accumulated score to the next player
+        
+        } else {
+            activePlayer = 0; // kapag si Player 2 ay '1' vice versa with Player 1 
+            roundScore = 0;   
 
-// for(let input of Inputs){
-//     console.log(input);  
-// }
+            playerCurrentScore.textContent = '0';  // reset current score for the next player's turn  
 
-//  // DOM manipulation 
- 
+            // toggling "active" adding if absent; removing if present 
+            player0Panel.classList.toggle('active');
+            player1Panel.classList.toggle('active'); 
+        }
+    }
+    console.log(nextPlayer); 
 
+/*********************** End of Taking Turns ****************************/ 
+
+/*********************** Start of HOLD function Button ****************************/ 
+    function holdScore(){
+        
+        // Assigning variables to target classes and id from html 
+        let currentPlayerGlobalScore = document.querySelector(`#score-${activePlayer}`); 
+        let currentPlayerScore = document.querySelector(`#current-${activePlayer}`); 
+        let currentPlayerName = document.querySelector(`#name-${activePlayer}`); 
+        let currentPlayerPanel = document.querySelector(`.player-${activePlayer}-panel`); 
+
+        if(gamePlay){
+            globalScores[activePlayer] += roundScore; // mag add yung roundScore at yung globalScores 
+            currentPlayerGlobalScore.textContent = globalScores[activePlayer]; // ididisplay yung globalScores ni active player 
+
+            if(globalScores[activePlayer] >= threshold.value){
+                currentPlayerName.textContent = "Winner!"; // papalitan name into "Winner!" text
+                currentPlayerScore.textContent = '0'; // reset to '0' current score 
+                imgDice.style.display = "none"; 
+
+                // Stop the game 
+                currentPlayerPanel.classList.remove('active'); 
+                currentPlayerPanel.classList.add('winner'); 
+                gamePlay = false;     
+
+            } else {
+                nextPlayer(); // another game will begin 
+            }
+        }
+    }
+
+/*********************** End of HOLD function Button ****************************/    
